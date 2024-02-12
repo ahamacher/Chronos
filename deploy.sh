4e474af72d2e335b316d7c0b42bc6d8a6e40fdbf
Andrew Hamacher <andrew.hamacher@gmail.com>
2024-02-12 11:31:40 -0800

    fix deploy to use user instead of root password

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 # 1. systemd configuration for this bot (might call run.sh)
 # 2. crontab which periodically checks to see if the bot is running, and if not it starts it
 
-rsync -av -e ssh --exclude='node_modules' --exclude='.env' ./* root@140.82.48.120:/root/bot-client/
-ssh root@140.82.48.120 'cd bot-client && pkill -9 run.sh ; tmux new-session -d ./run.sh'
+rsync -av -e ssh --exclude='node_modules' --exclude='.env' ./* andrewhamacher@140.82.48.120:/var/projects/bot-client/
+
+# ssh andrewhamacher@140.82.48.120 'cd /var/projects/bot-client && pkill -9 run.sh ; tmux new-session -d ./run.sh'
 # ssh root@140.82.48.120 systemctl restart bot
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 // application imports
-import * as dotenv from "dotenv";
-import "dotenv";
-dotenv.config();
+import * as dotenv from "dotenv"
+import "dotenv"
+dotenv.config()
 
-import { discordClient } from "./discordClient/";
-import { setSlashCommands } from "./discordClient/deployCommands";
-import { startServer } from "./backend";
+import { discordClient } from "./discordClient/"
+import { setSlashCommands } from "./discordClient/deployCommands"
 
-const token = process.env.DISCORD_TOKEN;
+const token = process.env.DISCORD_TOKEN
 
 discordClient.login(token).then(async () => {
-  await setSlashCommands();
-  startServer();
-  console.log("Ready to Rock!");
-});
+  await setSlashCommands()
+  console.log("Ready to Rock!")
+})
